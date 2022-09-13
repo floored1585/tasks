ad68471dfadf0ce6334d43757fc27647ce179725
EC2 Default User <ec2-user@ip-10-250-1-250.ec2.internal>
2022-09-13 19:16:13 +0000

    documentation

--- task1/cleanup.sh
+++ task1/cleanup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# The find command lets you specify (and negate) a pattern to match, as well
+# as allow filtering based on accessed/modified/created times. Most systems
+# don't keep track of last access time by default, though. find also contains
+# a -delete flag useful for this task, which will delete the matching files.
 find ~ec2-user/do-not-delete/ -atime +30 ! -name "*do-not-delete*" -delete
 
 # Alternative using -exec
--- task1/saverecord.sh
+++ task1/saverecord.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+
+# dig +short will return only the value of a record, which
+# is then redirected to the desired file. tee would also work
+# and let you display the output and execute the dig regardless
+# of the writability of the output file.
 dig +short challenging.rocketmiles.net TXT > ~ec2-user/dns.txt
